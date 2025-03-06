08adeaccb258177bdaa33e854ddf6a9aa2ca1b65
Mario Haim <MarHai@users.noreply.github.com>
2025-03-06 13:52:52 +0200

    Create analysis.r

---
+++ analysis.r
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+firstnames_female <- 
+  'etunimitilasto-2025-02-04-dvv_naiset-kaikki.csv' %>% 
+  read_csv2(col_types = 'cn')
+
+firstnames_male <- 
+  'etunimitilasto-2025-02-04-dvv_miehet-kaikki.csv' %>% 
+  read_csv2(col_types = 'cn')
+
+firstnames_female %>% 
+  slice_max(order_by = Lukumäärä)
+
+firstnames_female %>% 
+  summarise(M = mean(Lukumäärä),
+            SD = sd(Lukumäärä))
+
+firstnames_male %>% 
+  slice_max(order_by = Lukumäärä)
+
+firstnames_male %>% 
+  summarise(M = mean(Lukumäärä),
+            SD = sd(Lukumäärä))
